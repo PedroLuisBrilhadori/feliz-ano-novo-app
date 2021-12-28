ea67cbe28a9f72cc81b607932b744e24242c6963
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-28 15:27:19 -0300

    refatcor: organiza melhor o projeto

--- server/mural/mural.module.ts
+++ server/mural/mural.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MuralController } from './mural.controller';
-import { MuralService } from './mural.service';
+import { MuralService } from './service';
 
 @Module({
   controllers: [MuralController],
