307591b56890b72cee8798ef9712beb570a63db5
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-28 15:33:48 -0300

    faet: adiciona firebase ao serviço

--- server/mural/service/mural.service.ts
+++ server/mural/service/mural.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
+import * as admin from 'firebase-admin';
+import { Reference } from '@firebase/database-types';
 @Injectable()
-export class MuralService {}
+export class MuralService implements OnModuleInit {
+  ref: Reference;
+
+  constructor() {
+    this.ref = admin.database().ref();
+  }
+
+  onModuleInit() {
+    this.ref.on('value', (snap) => snap.val());
+  }
+}
