f8ff89b73adae05dee485a274ada901dcceebe43
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-28 15:11:39 -0300

    feat: adiciona firebase

---
+++ src/firebase.config.ts
@@ -0,0 +1,11 @@
+import { ServiceAccount } from 'firebase-admin';
+import * as admin from 'firebase-admin';
+
+export function InitFirebase() {
+  const adminConfig: ServiceAccount = JSON.parse(process.env.FIREBASE);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(adminConfig),
+    databaseURL: 'https://link-enc-default-rtdb.firebaseio.com',
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { InitFirebase } from './firebase.config';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+InitFirebase();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
